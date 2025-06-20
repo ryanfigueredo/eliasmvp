1200dd1e14fa60777e7f2638b2659bd943278401
Ryan Figueredo <ryan.figueredo059@gmail.com>
2025-06-20 17:42:06 -0300

    fix(api): remove public-read ACL from S3 upload configuration

--- src/app/api/documentos-client/upload.ts
+++ src/app/api/documentos-client/upload.ts
@@ -53,7 +53,6 @@ export default async function handler(
             Key: filename,
             Body: buffer,
             ContentType: info.mimeType,
-            ACL: 'public-read',
           }),
         )
 
